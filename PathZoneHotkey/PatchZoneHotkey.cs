d34ab17f61d01b8dfd88d744f6ed241a8e718b61
D.Mentia <dimencia@gmail.com>
2020-04-29 20:50:31 -0400

    Add project files.

---
+++ PathZoneHotkey/PatchZoneHotkey.cs
@@ -0,0 +1,34 @@
+using System;
+using PatchZone.Hatch;
+using PatchZone.Hatch.Utils;
+using PatchZoneHotkey.Services;
+
+using ECS;
+using Service.Achievement;
+using Service.Building;
+using Service.Localization;
+using Service.Street;
+using Service.UserWorldTasks;
+using HarmonyLib;
+
+namespace PatchZoneHotkey
+{
+    public class PatchZoneHotkey : Singleton<PatchZoneHotkey>, IPatchZoneMod
+    {
+        public static IPatchZoneContext Context { get; private set; }
+        public static Harmony Harmony;
+
+        public void Init(IPatchZoneContext context)
+        {
+            Context = context;
+            Harmony = new Harmony("PatchZoneHotkey");
+        }
+
+        public void OnBeforeGameStart()
+        {
+            Context.Log.Log("Adding PatchZone Hotkey");
+            Harmony.PatchAll();
+            Context.Log.Log("PatchZone Hotkey added");
+        }
+    }
+}
---
+++ PathZoneHotkey/PatchedMainMenu.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using Service.UI;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserInterface;
+
+namespace PatchZoneHotkey
+{
+    [HarmonyPatch(typeof(UIBase), "Tick")]
+    class PatchedMainMenu
+    {
+        private static string workingDir = $"{Environment.CurrentDirectory}{Path.DirectorySeparatorChar}PatchZone";
+        private static string path = $"{workingDir}{Path.DirectorySeparatorChar}PatchZone.GUI.exe";
+        public static void Postfix(UIBase __instance)
+        {
+            if (__instance is UIMainMenu && ServiceMapper.userInputService.ButtonDown(Service.UserInput.Button.HideUI))
+            {
+                // Open PatchZone form... this will take away focus so we don't have to worry about opening it multiple times
+                PatchZoneHotkey.Context.Log.Log($"Loading {path}");
+                // This feels gross but it does the job.  Otherwise it has the wrong base folder and is a different instance
+                var temp = Environment.CurrentDirectory;
+                Environment.CurrentDirectory = workingDir;
+                Process.Start(path);
+                Environment.CurrentDirectory = temp;
+            }
+            
+        }
+    }
+}
